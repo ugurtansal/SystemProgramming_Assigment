7cc2b80c64fe7911481910c74181023c30feed40
Ugur Tansal <ugur.tansal@gmail.com>
2024-04-07 13:31:30 +0300

    customer file

---
+++ customer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include "customer.h"
+extern char *foodsTxt;
+extern char *ordersTxt;
+extern char *closedOrdersTxt;
+extern char *takenOrdersTxt;
+extern char *logTtxt;
---
+++ customer.h
@@ -0,0 +1,5 @@
+#ifndef CUSTOMER_H_INCLUDED
+#define CUSTOMER_H_INCLUDED
+#include "common.h"
+
+#endif // CUSTOMER_H_INCLUDED
